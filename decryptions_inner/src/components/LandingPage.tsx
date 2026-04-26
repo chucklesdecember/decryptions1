5e4bdfbcbdf1408d91ca250594be02f886f46d91
Charlie November <charlie.november@duke.edu>
2026-04-26 18:38:52 -0400

    landingpage typo 4/26/26

--- decryptions_inner/src/components/LandingPage.tsx
+++ decryptions_inner/src/components/LandingPage.tsx
@@ -88,7 +88,7 @@ export function LandingPage({ onStartGame, onOpenArchive }: LandingPageProps) {
         </div>
 
         <div className="text-sm text-gray-700">
-          <p>New puzzles will not be made until after finals...expect a new puzzle on April 30th.</p>
+          <p>No new puzzles will be made until after finals...expect a new puzzle on April 30th.</p>
         </div>
 
       </div>
